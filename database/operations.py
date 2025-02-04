852fa135f626a71c59d8291f3f6c088e9e3ca625
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-04 09:54:34 +0100

    Update operations.py

--- database/operations.py
+++ database/operations.py
@@ -1,1 +1,46 @@
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+from os import getenv
+import urllib.parse
+from pyodbc import connect
+from pypyodbc import driver as odbc_driver
+
+
+# Load the environment variables
+load_dotenv()
+
+
+# Define the connection string using the environment variables
+server = os.getenv('AZURE_SQL_SERVER')
+database = os.getenv('AZURE_SQL_DATABASE')
+username = os.getenv('AZURE_SQL_USERNAME')
+password =  os.getenv('AZURE_SQL_PASSWORD')
+driver = os.getenv('AZURE_SQL_DRIVER')
+
+
+# Create the connection string
+connection_string = f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver={urllib.parse.quote_plus(driver)}&TrustServerCertificate=yes'
+
+# Create the engine
+engine = create_engine(connection_string)
+
+# Test the connection
+try:
+    with engine.connect() as connection:
+        print("Connection successful!")
+except Exception as e:
+    print(f"Connection failed: {e}")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
