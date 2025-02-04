d4e6f999c29aef1477420b5c8c0805ef53066536
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-05 00:29:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
@@ -7,68 +8,6 @@ import plotly.figure_factory as ff
 import plotly.offline as pyo
 import yfinance as yf
 
-'''
-FIX DATABASE CONNECTION LATER, PRESUME WITH CSV FILES FROM DATABASE
-
-import streamlit as st
-import pyodbc as odbc_driver
-import sqlalchemy
-from sqlalchemy import create_engine
-
-
-# Load credentials from secrets
-credentials = {
-    'server': st.secrets['AZURE_SQL_SERVER'],
-    'database': st.secrets['AZURE_SQL_DATABASE'],
-    'username': st.secrets['AZURE_SQL_USERNAME'],
-    'password': st.secrets['AZURE_SQL_PASSWORD'],
-   
-   
-}
-
-# Initialize connection
-@st.cache_resource
-def init_connection():
-    try:
-        conn_str = (f"Server=tcp:myfreesqldbserverjonny.database.windows.net,1433;"+
-            "Initial Catalog=myFreeDB;"+
-            "driver=ODBC Driver 17 for SQL Server"+
-            "Persist Security Info=False;"+
-            "User ID={username};"+
-            "MultipleActiveResultSets=False;"+
-            "Encrypt=True;"+
-            "TrustServerCertificate=False;"+
-            "Authentication=Active Directory Integrated"
-            )
-            
-        conn = pyodbc.connect(conn_str)
-        st.write("✅ Connection successful!")
-        return conn
-    except Exception as e:
-        st.error(f"🚨 Connection failed: {e}")
-        st.error(f"Connection string used: {conn_str}")
-        return None
-
-conn = init_connection()
-
-# Perform query
-@st.cache_data(ttl=600)
-def run_query(query):
-    if conn:
-        with conn.cursor() as cur:
-            cur.execute(query)
-            return cur.fetchall()
-    return []
-
-# Run SQL Query
-rows = run_query("SELECT * FROM mytable;")
-
-# Print results
-if rows:
-    for row in rows:
-        st.write(f"{row[0]} has a :{row[1]}:")
-
-'''
 
 st.titel("ETF Finder")
 
