702857bcc77c0f954f4c58fb25088c56a7c1c5d3
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-04 16:04:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -32,9 +32,10 @@ def connect_to_server():
 
 
 st.markdown('## ETF Overview and Comparison Tool')
-st.markdown(""" # /Disclaimer: Data provided by this app is not an official source. I can not guarantee for itsquality and actuality, 
-    so take it witha grain of salt. Never use this app soleily to actually make financial decisiions with real money!""")
+st.markdown(" ## __Disclaimer: Data provided by this app is not an official source. I can not guarantee for itsquality and actuality, so take it witha grain of salt. Never use this app soleily to actually make financial decisiions with real money!__")
 
+st.divider()
+st.markdown('## ETF Overview and Comparison Tool')
 # Test the connection
 st.text("Click on load database to connect with SQL-Server")
 st.button(label="Load Database", type="primary", on_click=connect_to_server)
