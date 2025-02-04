de6f1362b6d4e6bd5b15bed830889068b225c0f9
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-04 10:01:11 +0100

    Update operations.py

--- database/operations.py
+++ database/operations.py
@@ -1,38 +1,9 @@
-from sqlalchemy import create_engine
-from dotenv import load_dotenv
-from os import getenv
-import urllib.parse
-from pyodbc import connect
-from pypyodbc import driver as odbc_driver
+# Store ETF data
+store_etf_data(etf_df, server, database, username, password)
 
 
-# Load the environment variables
-load_dotenv()
-
-
-# Define the connection string using the environment variables
-server = os.getenv('AZURE_SQL_SERVER')
-database = os.getenv('AZURE_SQL_DATABASE')
-username = os.getenv('AZURE_SQL_USERNAME')
-password =  os.getenv('AZURE_SQL_PASSWORD')
-driver = os.getenv('AZURE_SQL_DRIVER')
-
-
-# Create the connection string
-connection_string = f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver={urllib.parse.quote_plus(driver)}&TrustServerCertificate=yes'
-
-# Create the engine
-engine = create_engine(connection_string)
-
-# Test the connection
-try:
-    with engine.connect() as connection:
-        print("Connection successful!")
-except Exception as e:
-    print(f"Connection failed: {e}")
-
-
-
+# Store Yahoo Finance data for SPY
+store_yahoo_data(spy, server, database, username, password)
 
 
 
