e90de091335ccc315fbbd010095da60ffbe911ba
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-04 10:35:03 +0100

    Update operations.py

--- database/operations.py
+++ database/operations.py
@@ -11,36 +11,10 @@ store_yahoo_data(spy, server, database, username, password)
 
 
 
-# Function to get the etf_df from the Azure SQL database
-from sqlalchemy import create_engine
-from dotenv import load_dotenv
-from os import getenv
-import urllib.parse
 
 
-# Load the environment variables
-load_dotenv()
 
 
-# Define the connection string using the environment variables
-server = os.getenv('AZURE_SQL_SERVER')
-database = os.getenv('AZURE_SQL_DATABASE')
-username = os.getenv('AZURE_SQL_USER')
-password =  os.getenv('AZURE_SQL_PASSWORD')
-driver = os.getenv('AZURE_SQL_DRIVER')
-
-# Create the connection string
-connection_string = f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver={urllib.parse.quote_plus(driver)}&TrustServerCertificate=yes'
-
-# Create the engine
-engine = create_engine(connection_string)
-
-# Test the connection
-try:
-    with engine.connect() as connection:
-        print("Connection successful!")
-except Exception as e:
-    print(f"Connection failed: {e}")
 
 def get_etf_data(server, database, username, password, table_name='etf_data'):
     connection_string = f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver=ODBC+Driver+18+for+SQL+Server'
