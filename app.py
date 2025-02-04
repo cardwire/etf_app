33caaa86541ae3f18b03d7b48643630c97df1d99
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-04 10:36:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,36 @@
 import streamlit as st
 from database.operations import get_etfs, get_etf_details
 from yfinance_integration.yfinance_api import get_etf_data
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+from os import getenv
+import urllib.parse
+
+# Load the environment variables
+load_dotenv()
+
+# Define the connection string using the environment variables
+server = os.getenv('AZURE_SQL_SERVER')
+database = os.getenv('AZURE_SQL_DATABASE')
+username = os.getenv('AZURE_SQL_USER')
+password =  os.getenv('AZURE_SQL_PASSWORD')
+driver = os.getenv('AZURE_SQL_DRIVER')
+
+# Create the connection string
+connection_string = f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver={urllib.parse.quote_plus(driver)}&TrustServerCertificate=yes'
+
+# Create the engine
+engine = create_engine(connection_string)
+
+# Test the connection
+try:
+    with engine.connect() as connection:
+        print("Connection successful!")
+except Exception as e:
+    print(f"Connection failed: {e}")
+
+
+
 
 st.title('ETF Overview and Comparison Tool')
 
