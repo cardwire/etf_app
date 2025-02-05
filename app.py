bf650886da4d08cb806314f852be64f07c79eb33
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-06 00:29:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,9 @@ import pandas as pd
 
 st.markdown("# ETF Finder")
 etf_df = pd.read_csv("database/etf_df.csv")
+etf_df.drop(columns = "maxAge", axis=1, inplace= True)
 
-# Drop 'max Age' column
-if 'max Age' in etf_df.columns:
-    etf_df = etf_df.drop(columns=['max Age'])
+
 
 # Add a checkbox column
 etf_df['select'] = False
@@ -26,12 +25,6 @@ def toggle_selection(symbol):
 
 st.text("Click once on a symbol to select, click again to deselect. Max 5 selections.")
 st.write("Selected ETFs:", st.session_state.selected_etfs)
-
-# Confirm button
-if st.button("Confirm Selection"):
-    confirmed_etfs = st.session_state.selected_etfs
-    st.write("You confirmed:", confirmed_etfs)
-    # Add logic to handle confirmed ETFs (e.g., display details)
 
 # Display the scrollable table with checkboxes
 for i, row in etf_df.iterrows():
