ddc89d609ea50ee7be96656a2db32bfade246212
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-09 22:25:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from umap import UMAP
 
 st.set_page_config(page_title="Page 1", page_icon=":page_facing_up:")
 
-st.sidebar.success("Select a demo above.")
+st.sidebar.success("Exploratory Data Analysis","3D Cluster Analysis", "Funds Analizer")
 
 st.markdown("# Page 1")
 st.write("Welcome to Page 1!")
