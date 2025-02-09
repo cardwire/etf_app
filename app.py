3093853ff4e84015f31f577f0e9a0b73398608ae
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-09 18:15:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 from sklearn.preprocessing import StandardScaler
 import umap
+import umap_learn
+from umap_learn import UMAP
 
 
 st.markdown("# ETF Finder")
