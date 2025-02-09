f743790e09c17c9e0a0044a02d8e9d79a79e3d21
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-09 22:44:03 +0100

    Update and rename page 1 to page 1.py

---
+++ page 1.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Page 1", page_icon=":page_facing_up:")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown("# Page 1")
+st.write("Welcome to Page 1!")
