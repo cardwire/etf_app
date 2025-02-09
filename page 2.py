fc219d3de6db9adf2d0e87e2e226562bc2ccbb4f
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-09 22:45:07 +0100

    Update and rename page 2 to page 2.py

---
+++ page 2.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Page 2", page_icon=":page_facing_up:")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown("# Page 2")
+st.write("Welcome to Page 2!")
