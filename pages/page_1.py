d1d9d64c7adc765fc07814affe0cd65c74b1015a
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-14 04:55:37 +0100

    Update page_1.py

--- pages/page_1.py
+++ pages/page_1.py
@@ -20,18 +20,6 @@ st.sidebar.success("Select a Page")
 page = st.sidebar.radio("Go to", ["Page 1", "Page 2", "Page 3"])
 
 
-# Load the ETF data
-
-if page == "app":
-    st.markdown("# Homepage")
-elif page == "Page 1":
-    switch_page("page_1.py")
-elif page == "Page 2":
-    switch_page("page_2.py")
-elif page == "Page 3":
-    switch_page("page_3.py")
-
-st.markdown("# ETF Finder")
 
 # Load the ETF data
 data = pd.read_excel("database/df.xlsx")
