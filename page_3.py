f26a6d13dbafe8ca9dd907280a20c6744c83e115
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-14 04:29:28 +0100

    Delete page_3.py

--- page_3.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Page 3", page_icon=":page_facing_up:")
-
-st.sidebar.success("Select a demo above.")
-
-st.markdown("# Page 3")
-st.write("Welcome to Page 3!")
