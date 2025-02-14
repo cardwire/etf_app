810334024f4fd2b5b164e9160b136907c5d52621
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-14 18:48:50 +0100

    Update operations.py

--- database/operations.py
+++ database/operations.py
@@ -1,15 +1,64 @@
 # Store ETF data
-store_etf_data(etf_df, server, database, username, password)
+#store_etf_data(etf_df, server, database, username, password)
 
 
 # Store Yahoo Finance data for SPY
-store_yahoo_data(spy, server, database, username, password)
+#store_yahoo_data(spy, server, database, username, password)
+
+
+# All functions required in this app
 
 
 
+#define the prophet forecast function
+def prophet_forecast(ticker, period):
+    #create the history data in prophet style
+    history = ticker.history(period='max')
+    history = history.reset_index()
+    history = history.rename(columns={'Date': 'ds', 'Close': 'y'})
+    history["ds"] = history['ds'].dt.tz_localize(None)
+    #create a prophet model                 
+    model = Prophet()
+    model.fit(history)
+    # create a future dataframe for the next 365 days
+    future = model.make_future_dataframe(periods=period)
+
+    # make predictions
+    forecast = model.predict(future)
+
+    # plot the forecast using plotly
+    fig = go.Figure()
 
 
 
+#define the adaboost forecast function
+#import numpy as np
+#from sklearn.ensemble import AdaBoostRegressor
+#from sklearn.tree import DecisionTreeRegressor
+
+def ada_forecast(ticker, period):
+    #create the history data in prophet style
+    history = ticker.history(period='max')
+    history = history.reset_index()
+    history = history.rename(columns={'Date': 'ds', 'Close': 'y'})
+    history["ds"] = history['ds'].dt.tz_localize(None)
+    #create a prophet model                 
+    X = np.array((history['ds'] - history['ds'].min()).dt.days).reshape(-1, 1)
+    y = history['y']
+    # create a future dataframe for the next 365 days
+    # Create and fit the AdaBoost model
+    model = AdaBoostRegressor(DecisionTreeRegressor(max_depth=4), n_estimators=100, random_state=42)
+    model.fit(X, y)
+
+    # Create future dates
+    future_dates = pd.date_range(start=history['ds'].max(), periods=period).to_frame(index=False, name='ds')
+    future_X = np.array((future_dates['ds'] - history['ds'].min()).dt.days).reshape(-1, 1)
+
+    # Make predictions
+    forecast = model.predict(future_X)
+
+    # plot the forecast using plotly
+    fig = go.Figure()
 
 
 
