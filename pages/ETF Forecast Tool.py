21086312531d58a7337258ff8e0d89604454e1e0
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-14 18:57:32 +0100

    Update ETF Forecast Tool.py

--- pages/ETF Forecast Tool.py
+++ pages/ETF Forecast Tool.py
@@ -10,47 +10,6 @@ from sklearn.ensemble import AdaBoostRegressor
 from sklearn.tree import DecisionTreeRegressor
 
 
-#define the forecast function
-def prophet_forecast(ticker, period):
-    #create the history data in prophet style
-    history = ticker.history(period='max')
-    history = history.reset_index()
-    history = history.rename(columns={'Date': 'ds', 'Close': 'y'})
-    history["ds"] = history['ds'].dt.tz_localize(None)
-    #create a prophet model                 
-    model = Prophet()
-    model.fit(history)
-    # create a future dataframe for the next 365 days
-    future = model.make_future_dataframe(periods=period)
-
-    # make predictions
-    forecast = model.predict(future)
-
-    # plot the forecast using plotly
-    fig = go.Figure()
-
-    # Add the actual data
-    fig.add_trace(go.Scatter(x=history['ds'], y=history['y'], mode='lines', name='Actual'))
-    # Add the forecast data
-    fig.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yhat'], mode='lines', name='Forecast'))
-
-    # Add the upper and lower bounds
-    fig.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yhat_upper'], mode='lines', name='Upper Bound', line=dict(dash='dash')))
-    fig.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yhat_lower'], mode='lines', name='Lower Bound', line=dict(dash='dash')))
-
-    # indicate the forecasted region with a vertical line at the last known date
-    fig.add_vline(x=history['ds'].max(), line_width=2, line_dash="dash", line_color="black")
-
-    #add slider to the plot to zoom in and out
-    fig.update_layout(xaxis_rangeslider_visible=True)
-
-    # Update layout
-    fig.update_layout(title=f'Forecast for {ticker.ticker} for the next {period} days',
-                      xaxis_title='Date',
-                      yaxis_title='Price')
-
-    st.plotly_chart(fig)
-
 # Set page configuration
 st.set_page_config(page_title="ETF Forecast Tool", page_icon=":chart_with_upwards_trend:")
 
