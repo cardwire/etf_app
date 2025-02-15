5e2c5f7be47b90b93274d3651695176d3ecf1f9a
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-15 08:27:52 +0100

    Update ETF 3D-Visualizer.py

--- pages/ETF 3D-Visualizer.py
+++ pages/ETF 3D-Visualizer.py
@@ -72,7 +72,7 @@ cat_columns = pd.get_dummies(cats_to_add).astype(int)
 data_final = pd.concat([data_scaled, cat_columns.reset_index(drop=True)], axis=1)
 
 # get labels for LDA
-labels = df['type']
+labels = data_final['type']
 lda_components = lda.fit_transform(df_numeric_imputed_scaled, labels)
 
 # Find the minimum value in the dataframe
