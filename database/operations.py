ffeab2d752a25665176a22bd4bb4af36a1ac98c2
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-15 14:59:55 +0100

    Update operations.py

--- database/operations.py
+++ database/operations.py
@@ -25,42 +25,43 @@ from prophet import Prophet
 
 
 #define the prophet forecast function
+from prophet import Prophet
+import plotly.graph_objects as go
+import streamlit as st
+import pandas as pd
+
 def prophet_forecast(ticker, period):
-    #create the history data in prophet style
+    # Create the history data in prophet style
     history = ticker.history(period='max')
     history = history.reset_index()
     history = history.rename(columns={'Date': 'ds', 'Close': 'y'})
     history['ds'] = pd.to_datetime(history['ds'])
     history["ds"] = history['ds'].dt.tz_localize(None)
 
-    #create a prophet model                 
+    # Create a prophet model                 
     model = Prophet()
     model.fit(history)
-    # create a future dataframe for the next 365 days
+    # Create a future dataframe for the next 365 days
     future = model.make_future_dataframe(periods=period)
 
-    # make predictions
+    # Make predictions
     forecast = model.predict(future)
 
-    # plot the forecast using plotly
+    # Plot the forecast using plotly
     fig = go.Figure()
-    #limit the timeperiod to the same as the forecast
-    history = data[data['ds'] <= forecast['ds'].max()]
+    # Limit the time period to the same as the forecast
+    history = history[history['ds'] <= forecast['ds'].max()]
     # Add the actual data
     fig.add_trace(go.Scatter(x=history['ds'], y=history['y'], mode='lines', name='Actual'))
     # Add the forecast data
     fig.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yhat'], mode='lines', name='Forecast'))
-
     # Add the upper and lower bounds
     fig.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yhat_upper'], mode='lines', name='Upper Bound', line=dict(dash='dash')))
     fig.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yhat_lower'], mode='lines', name='Lower Bound', line=dict(dash='dash')))
-
-    # indicate the forecasted region with a vertical line at the last known date
+    # Indicate the forecasted region with a vertical line at the last known date
     fig.add_vline(x=history['ds'].max(), line_width=2, line_dash="dash", line_color="black")
-
-    #add slider to the plot to zoom in and out
+    # Add slider to the plot to zoom in and out
     fig.update_layout(xaxis_rangeslider_visible=True)
-
     # Update layout
     fig.update_layout(title=f'Forecast for {ticker.ticker} for the next {period} days',
                       xaxis_title='Date',
