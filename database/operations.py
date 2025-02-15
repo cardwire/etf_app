6850b96494b131d72e8f12c61a3f131d4af93776
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-15 14:09:53 +0100

    Update operations.py

--- database/operations.py
+++ database/operations.py
@@ -1,6 +1,11 @@
 # Store ETF data
 #store_etf_data(etf_df, server, database, username, password)
-
+import pandas as pd
+def add_esg(data, esg):
+    esg = pd.read_csv("database/esg.csv")
+    data = data.merge(esg, left_on="symbol", right_on="ticker", how="left")
+    return data
+    
 
 # Store Yahoo Finance data for SPY
 #store_yahoo_data(spy, server, database, username, password)
