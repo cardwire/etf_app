e1643af87c1daa98363ea11965c9e5bbe7228109
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-16 13:54:40 +0100

    Update latest.py

--- pages/latest.py
+++ pages/latest.py
@@ -9,7 +9,8 @@ st.set_page_config(page_title="Latest Actions and current Performance", page_ico
 
 st.markdown("# Select an ETF from the interactive Table. Or directly select your ETF of choice via searching its Tickersymbol")
 
-data = pd.read_csv("df_esg.csv", indexcol=False)
+
+data = pd.read_csv("df_esg.csv", index_col=False)
 st.dataframe("data")
 st.write()
 st.text_input()
