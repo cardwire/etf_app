b6810fc4d306840671785198e4c7b72e5a238603
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-16 14:12:47 +0100

    Update latest.py

--- pages/latest.py
+++ pages/latest.py
@@ -52,6 +52,6 @@ if selected_etfs:
     long_sum = ticker.info['longBusinessSummary']
 
     st.markdown(f"## {symbol}:")
-    st.markdown(f"### Read this general information on your chosen ETF: {long_sum}")
+    st.markdown(f"### {long_sum}")
 
     st.divider()
