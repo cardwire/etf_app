379dc3eaa3e244a01fa159da3fa7cda5a4dde2e6
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-16 17:07:12 +0100

    Update latest.py

--- pages/latest.py
+++ pages/latest.py
@@ -7,12 +7,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import yfinance as yf
 
-
-
 st.set_page_config(page_title="Latest Actions and current Performance", page_icon="📊")
 
 st.markdown("# Select an ETF from the interactive Table. Or directly select your ETF of choice via searching its Tickersymbol")
-
 
 # Initialize session state for selected ETFs
 if 'selected_etfs' not in st.session_state:
@@ -35,7 +32,7 @@ edited_data = st.data_editor(data, column_config={
 # Update selected ETFs in session state
 selected_etfs = edited_data[edited_data['Select']]['symbol'].tolist()
 
-# Limit selection to 1 ETFs
+# Limit selection to 1 ETF
 if len(selected_etfs) > 1:
     st.warning("You can select only one ETF to forecast its performance.")
     selected_etfs = selected_etfs[:1]
@@ -52,7 +49,6 @@ if selected_etfs:
     # get long business summary
     long_sum = ticker.info['longBusinessSummary']
 
-   
     st.markdown(f" ## {ticker} Factsheet")
     col1, col2, col3 = st.columns(3)
 
@@ -62,12 +58,11 @@ if selected_etfs:
 
     with col2:
         st.write("KPIs")
-        st.markdown(f" return (year to day): {ticker.info('ytdReturn')}")
-        st.markdown(f" current bid: {ticker.info('bid')}")
-        st.markdown(f" current bidsize: {ticker.info('bidSize')}")
-        st.markdown(f" current ask: {ticker.info('ask')}")
-        st.markdown(f" current asksize: {ticker.info('askSize')}")
-        
+        st.markdown(f" return (year to day): {ticker.info['ytdReturn']}")
+        st.markdown(f" current bid: {ticker.info['bid']}")
+        st.markdown(f" current bidsize: {ticker.info['bidSize']}")
+        st.markdown(f" current ask: {ticker.info['ask']}")
+        st.markdown(f" current asksize: {ticker.info['askSize']}")
 
     with col3:
         st.write("")
@@ -77,43 +72,43 @@ if selected_etfs:
 
 st.divider()
 
-        st.header("last 5 day performance")
-        ticker_hist = ticker.history(period='5d', interval='1m')
-        ticker_hist['Date'] = ticker_hist.index
-        ticker_hist.reset_index(drop=True, inplace=True)
-        ticker_hist['Date'] = pd.to_datetime(ticker_hist['Date'])
-        ticker_hist['Date'] = ticker_hist['Date'].dt.strftime('%Y-%m-%d %H:%M:%S')
-        
-        # Plotting the data as a candlestick chart using Plotly
-        fig = go.Figure(data=[go.Candlestick(x=ticker_hist.index,
-                                             open=ticker_hist['Open'],
-                                             high=ticker_hist['High'],
-                                             low=ticker_hist['Low'],
-                                             close=ticker_hist['Close'])])
+st.header("Last 5 Day Performance")
+ticker_hist = ticker.history(period='5d', interval='1m')
+ticker_hist['Date'] = ticker_hist.index
+ticker_hist.reset_index(drop=True, inplace=True)
+ticker_hist['Date'] = pd.to_datetime(ticker_hist['Date'])
+ticker_hist['Date'] = ticker_hist['Date'].dt.strftime('%Y-%m-%d %H:%M:%S')
 
-        fig.update_layout(title='5 day performance of GGG',
-                          xaxis_title='Date',
-                          yaxis_title='Price',
-                          xaxis_rangeslider_visible=False)
-        
-        # shift titel to center
-        fig.update_layout(title_x=0.5)
-        #reshape x-axis to range(1, 5 days)
-        fig.update_xaxes(tickvals=np.arange(0, len(ticker_hist), step=78), ticktext=np.arange(0, 6))
-        fig.update_xaxes(tickangle=45)
-        fig.update_yaxes(tickprefix='$')
-        fig.update_layout(showlegend=False)
-        fig.update_layout(width=800, height=600)
-        fig.update_layout(autosize=False)
-        fig.update_layout(margin=dict(l=50, r=50, t=50, b=50))
-        fig.update_layout(plot_bgcolor='white')
-        fig.update_layout(paper_bgcolor='white')
-        fig.update_layout(xaxis=dict(showgrid=True, gridwidth=1, gridcolor='lightgrey'))
-        fig.update_layout(yaxis=dict(showgrid=True, gridwidth=1, gridcolor='lightgrey'))       
-        fig.update_layout(font=dict(family='Arial', size=12, color='black'))
-        fig.update_layout(title_font=dict(size=20))
-        fig.update_layout(title_font_size=20)
-        fig.update_layout(title_font_family='Arial')
-        fig.update_layout(title_font_color='black')
-        
-        st.plotly_chart(fig)
+# Plotting the data as a candlestick chart using Plotly
+fig = go.Figure(data=[go.Candlestick(x=ticker_hist.index,
+                                     open=ticker_hist['Open'],
+                                     high=ticker_hist['High'],
+                                     low=ticker_hist['Low'],
+                                     close=ticker_hist['Close'])])
+
+fig.update_layout(title='5 Day Performance of GGG',
+                  xaxis_title='Date',
+                  yaxis_title='Price',
+                  xaxis_rangeslider_visible=False)
+
+# Shift title to center
+fig.update_layout(title_x=0.5)
+# Reshape x-axis to range(1, 5 days)
+fig.update_xaxes(tickvals=np.arange(0, len(ticker_hist), step=78), ticktext=np.arange(0, 6))
+fig.update_xaxes(tickangle=45)
+fig.update_yaxes(tickprefix='$')
+fig.update_layout(showlegend=False)
+fig.update_layout(width=800, height=600)
+fig.update_layout(autosize=False)
+fig.update_layout(margin=dict(l=50, r=50, t=50, b=50))
+fig.update_layout(plot_bgcolor='white')
+fig.update_layout(paper_bgcolor='white')
+fig.update_layout(xaxis=dict(showgrid=True, gridwidth=1, gridcolor='lightgrey'))
+fig.update_layout(yaxis=dict(showgrid=True, gridwidth=1, gridcolor='lightgrey'))
+fig.update_layout(font=dict(family='Arial', size=12, color='black'))
+fig.update_layout(title_font=dict(size=20))
+fig.update_layout(title_font_size=20)
+fig.update_layout(title_font_family='Arial')
+fig.update_layout(title_font_color='black')
+
+st.plotly_chart(fig)
