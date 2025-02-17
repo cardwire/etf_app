ce96e0b42313c30b636d907eeaf2c960b91ffadd
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-17 03:15:27 +0100

    Update ETF 3D-Visualizer.py

--- pages/ETF 3D-Visualizer.py
+++ pages/ETF 3D-Visualizer.py
@@ -143,17 +143,17 @@ if st.button("Launch 3D Visualizer"):
     hover_data = data[['symbol', 'ytd_return', 'total_assets', 'fifty_day_average', 'bid', 'ask', 'category']].copy()
     data_with_hover = pd.concat([data_embeddings.reset_index(drop=True), hover_data.reset_index(drop=True)], axis=1)
   
-        # 3D Scatter plot
-        if data_embeddings.shape[1] >= 3:
-            fig = px.scatter_3d(data_with_hover, 
-                                x=data_embeddings.columns[0], 
-                                y=data_embeddings.columns[1], 
-                                z=data_embeddings.columns[2], 
-                                color=labels,
-                                hover_data=hover_data.columns, 
-                                title=f"3D {dimensionality_reduction_method} Clustering of ETFs")
-            fig.update_traces(marker=dict(size=2.5), opacity=0.8)
-            st.plotly_chart(fig)
-        else: 
-            st.error("Selected method does not produce enough components for 3D visualization.")
+    # 3D Scatter plot
+    if data_embeddings.shape[1] >= 3:
+        fig = px.scatter_3d(data_with_hover, 
+                            x=data_embeddings.columns[0], 
+                            y=data_embeddings.columns[1], 
+                            z=data_embeddings.columns[2], 
+                            color=labels,
+                            hover_data=hover_data.columns, 
+                            title=f"3D {dimensionality_reduction_method} Clustering of ETFs")
+        fig.update_traces(marker=dict(size=2.5), opacity=0.8)
+        st.plotly_chart(fig)
+    else: 
+        st.error("Selected method does not produce enough components for 3D visualization.")
    
