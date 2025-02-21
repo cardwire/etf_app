ec3ac204afeb4452abc962d9bc55cde05f80016e
cardwire <135005986+cardwire@users.noreply.github.com>
2025-02-21 08:39:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from streamlit_extras.switch_page_button import switch_page
 
 # Load the ETF data
 data = pd.read_excel("database/df.xlsx")
+esg = pd.read_excel("database/esg_data.xlsx")
 
 # Preprocess missing values
 data['type'] = data['type'].fillna('Other').replace('-', 'Other')
